3c35791e962d2d30af04293fe4665587d3cd4ab6
CEU-UIA <ceu@uia.org.ar>
2026-01-21 10:53:52 -0300

    Update macro_home.py

--- pages/macro_home.py
+++ pages/macro_home.py
@@ -1,25 +1,32 @@
 import streamlit as st
 
+
+def render_macro_home(go_to):
     st.markdown(
         """
         <div class="home-wrap">
-          <div class="home-title">Macroeconomía</div>
-          <div class="home-subtitle">Seleccioná una variable</div>
+            <div class="home-title">Macroeconomía</div>
+            <div class="home-subtitle">Seleccioná una variable</div>
         </div>
         """,
         unsafe_allow_html=True,
     )
 
     left_pad, mid, right_pad = st.columns([1, 6, 1])
+
     with mid:
         st.markdown('<div class="home-cards">', unsafe_allow_html=True)
+
         c1, c2, c3 = st.columns(3)
+
         with c1:
             if st.button("💱\nTipo de cambio", use_container_width=True):
                 go_to("macro_fx")
+
         with c2:
             if st.button("📈\nTasa de interés", use_container_width=True):
                 go_to("macro_tasa")
+
         with c3:
             if st.button("🛒\nPrecios", use_container_width=True):
                 go_to("macro_precios")
